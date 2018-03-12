34a0ef7ad7a96decfa3d87d91dc03129e74f649e
heidfeld <l.karmanski@gmail.com>
2018-03-12 23:22:30 +0100

    Valid layout of front-end

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node.js application.' });
+  res.render('index', { title_left: 'Node.js Algorithm', title_right: 'Node.js Requests Strike' });
 });
 
 module.exports = router;
