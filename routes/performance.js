4c70f5da90550a2f19be044c533a97cba683a554
heidfeld <l.karmanski@gmail.com>
2018-03-21 21:50:49 +0100

    refactor sources

--- routes/performance.js
+++ routes/performance.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+      {name: 'test1', status: 'ok'},
+      {name: 'test2', status: 'ok'},
+      {name: 'test3', status: 'ok'},
+      {name: 'test4', status: 'ok'}
+      ]);
 });
 
 module.exports = router;
