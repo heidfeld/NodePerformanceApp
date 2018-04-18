8e29b90afb4d8a756be56abc98a2f99f78a1bb92
heidfeld <l.karmanski@gmail.com>
2018-04-18 22:37:53 +0200

    working dijkstra

--- routes/performance.js
+++ routes/performance.js
@@ -2,34 +2,24 @@ var express = require('express');
 var router = express.Router();
 //var Entity = require('../utils/db.js')
 var Entity = require('../utils/dijkstra_db.js')
-var dijkstra = require('../utils/dijkstra.js')
+var graph = require('../utils/dijkstra.js')
+
 
 router.get('/', function(req, res) {
-    var result = Entity.find(function (err, data) {
+    Entity.find(function (err, data) {
         if (err) return console.error(err);
 
-        var g = new Graph();
+        var g = new graph();
+        data.forEach(function (item) {
+           g.addVertex(item.name, item.nodes);
+        });
 
-        g.addVertex('A', {B: 7, C: 8});
-        g.addVertex('B', {A: 7, F: 2});
-        g.addVertex('C', {A: 8, F: 6, G: 4});
-        g.addVertex('D', {F: 8});
-        g.addVertex('E', {H: 1});
-        g.addVertex('F', {B: 2, C: 6, D: 8, G: 9, H: 3});
-        g.addVertex('G', {C: 4, F: 9});
-        g.addVertex('H', {E: 1, F: 3});
+        let result = g.shortestPath('Node_2', 'Node_7').concat(['Node_2']).reverse();
 
-// Log test, with the addition of reversing the path and prepending the first node so it's more readable
-        console.log(g.shortestPath('A', 'H').concat(['A']).reverse());
+        console.log(result);
 
-
-
-
-        res.json(data);
+        res.json(result);
     })
-
-
-
 });
 
 module.exports = router;
